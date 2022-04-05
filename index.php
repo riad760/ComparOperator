47c5a2b9d7e50bef4cada00aefb6a819e9dc2375
riad760 <benalr760@gmail.com>
2022-04-05 11:45:08 +0200

    index++

--- index.php
+++ index.php
@@ -44,82 +44,94 @@ __
                 </div>
             </div>
         </nav>
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-            <div class="col">
-                <div class="container">
-                    <div class="card">
-                        <img src="./image/londre.jpg" class="card-img-top" alt="londre">
-                        <div class="card-body">
-                            <h5 class="card-title">londre</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
+        <div class="row row-cols ">
+            <div class="card" style="width: 20rem;">
+                <img src="./image/londre.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ville de londre</h5>
+                    <p class="card-text">Londres est connue pour être l’une des plus belle ville du monde,<br>
+                        mais aussi l’une des plus diversifiée en terme de culture avec 50 nationalités parlant plus de 300 langues différentes. <br>
+                        La ville légendaire est aussi renommée pour la richesse de son histoire et de sa culture qui s’étalent sur presque 2000 ans..</p>
+                    <center> <a href="#" class="btn btn-primary">visiter</a></center>
                 </div>
             </div>
-            <div class="col">
+            <div class="card" style="width: 20rem;">
+                <img src="./image/rome.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ville de rome</h5>
+                    <p class="card-text">Colisée, le monument de Rome
+                        Le haut lieu touristique numéro 1 de Rome est le Colisée,<br>
+                        c’est-a-dire l’énorme amphithéâtre qui pouvait accueillir plus de 65 000 spectateurs à l’époque des Romains.<br>
+                        Dans l’arène, les gladiateurs se battaient entre eux et devaient affronter des animaux sauvages.<br>
+                    </p>
+                    <center>
+                        <a href="#" class="btn btn-primary">visiter</a>
+                    </center>
+                </div>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <img src="./image/londre.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ville de monaco</h5>
+                    <p>Monaco-Ville (monégasque : Mu̍negu-Vila) est un quartier de Monaco.
+
+                        C'est la partie la plus ancienne de la principauté, que l'on appelle aussi communément « Rocher de Monaco »<br>
+                        (monégasque : Roca de Mu̍negu) voire, localement, « le Rocher » (monégasque : a Roca),<br>
+                        dénomination favorite de la presse à sensation. <br>
+
+                    </p>
+                    <center>
+                        <a href="#" class="btn btn-primary">visiter</a>
+                    </center>
+                </div>
+            </div>
+            <div class="card" style="width: 20rem;">
+                <img src="./image/monaco.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">ville de tunis</h5>
+                    <p>
+                        Tunis est l'immense capitale de la Tunisie, un pays d'Afrique du Nord.<br>
+                        Elle se trouve le long du lac de Tunis, juste en retrait du golfe de Tunis, sur la mer Méditerranée. <br>
+                        Elle accueille une médina centenaire et le Bardo
+                    </p>
+                    <center>
+                        <a href="#" class="btn btn-primary">visiter</a>
+                    </center>
+                </div>
+            </div>
+            <footer>
                 <div class="container">
-                    <div class="card">
-                        <img src="./image/monaco.jpg" class="card-img-top" alt="ville de monaco">
-                        <div class="card-body">
-                            <h5 class="card-title">monaco</h5>
-                            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col">
-                    <div class="container">
-                        <div class="card">
-                            <img src="./image/rome.jpg" class="card-img-top" alt=".ville de rome..">
-                            <div class="card-body">
-                                <h5 class="card-title">rome</h5>
-                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="container">
-                            <div class="card">
-                                <img src="./image/Tunis.jpg" class="card-img-top" alt=".ville de tunis..">
-                                <div class="card-body">
-                                    <h5 class="card-title">tunis</h5>
-                                    <p class="card-text"></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <footer>
-                        <div class="container">
-                            <div class="footer-dark">
+                    <div class="footer-dark">
 
-                                <div class="row">
-                                    <div class="col-sm-6 col-md-3 item">
-                                        <h3>Services</h3>
-                                        <ul>
-                                            <li><a href="#">Web design</a></li>
-                                            <li><a href="#">Development</a></li>
-                                            <li><a href="#">Hosting</a></li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-md-2 col-md-3 item">
-                                        <h3>a propos de nous </h3>
-                                        <ul>
-                                            <li><a href="#">Companie</a></li>
-                                            <li><a href="#">equipe</a></li>
-
-                                        </ul>
-                                    </div>
-                                    <div class="col-md-6 item text">
-                                        <h3>RF operator</h3>
-                                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
-                                    </div>
-                                    <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
-                                </div>
-                                <p class="copyright">Company Name © 2018</p>
+                        <div class="row">
+                            <div class="col-sm-6 col-md-3 item">
+                                <h3>Services</h3>
+                                <ul>
+                                    <li><a href="#">Web design</a></li>
+                                    <li><a href="#">Development</a></li>
+                                    <li><a href="#">Hosting</a></li>
+                                </ul>
                             </div>
-                    </footer>
-                </div>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+                            <div class="col-md-2 col-md-3 item">
+                                <h3>a propos de nous </h3>
+                                <ul>
+                                    <li><a href="#">Companie</a></li>
+                                    <li><a href="#">equipe</a></li>
+
+                                </ul>
+                            </div>
+                            <div class="col-md-6 item text">
+                                <h3>RF operator</h3>
+                                <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo.</p>
+                            </div>
+                            <div class="col item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a></div>
+                        </div>
+                        <p class="copyright">Company Name © 2018</p>
+                    </div>
+            </footer>
+        </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
     </body>
 
 </html>
